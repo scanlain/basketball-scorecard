3f0d3479122948ab836049d13576072dd932b860
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2026-01-08 15:58:27 +0530

    updated the js file

--- index.js
+++ index.js
@@ -1,69 +1,59 @@
-const IncrementBtns = document.querySelectorAll('.score-btn')
-const resetScoreBtn = document.querySelector('#reset-score-btn')
+document.addEventListener("DOMContentLoaded", function () {
+  const IncrementBtns = document.querySelectorAll(".score-btn");
+  const resetScoreBtn = document.querySelector("#reset-score-btn");
 
-const homeSection = document.querySelector('#home')
-const guestSecton = document.querySelector('#guest')
+  const homeSection = document.querySelector("#home");
+  const guestSecton = document.querySelector("#guest");
 
-let homeScore = 0
-let guestScore = 0
+  let homeScore = 0;
+  let guestScore = 0;
 
-function updateLeaderStyles(){
+  function updateLeaderStyles() {
+    homeSection.style.backgroundColor = "blanchedalmond";
+    guestSecton.style.backgroundColor = "blanchedalmond";
 
-    homeSection.style.backgroundColor = 'blanchedalmond'
-    guestSecton.style.backgroundColor = 'blanchedalmond'
-
-    if(homeScore > guestScore){
-        homeSection.style.backgroundColor = 'green'
+    if (homeScore > guestScore) {
+      homeSection.style.backgroundColor = "green";
+    } else if (guestScore > homeScore) {
+      guestSecton.style.backgroundColor = "green";
     }
-    else if(guestScore > homeScore){
-        guestSecton.style.backgroundColor = 'green'
-    }
-}
+  }
 
-IncrementBtns.forEach((btn) => {
+  IncrementBtns.forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      let clickedBtn = event.target;
+      console.log("clicked btn :", clickedBtn);
 
-    btn.addEventListener('click', function(event){
+      console.log("clicked btn text :", clickedBtn.textContent);
 
-        let clickedBtn = event.target
-        console.log('clicked btn :', clickedBtn)
+      let NumberToIncrement = Number(clickedBtn.textContent.trim());
+      console.log("score to increase : ", NumberToIncrement);
 
-        console.log('clicked btn text :', clickedBtn.textContent)
-        
-        let NumberToIncrement = Number(clickedBtn.textContent.trim())
-        console.log('score to increase : ', NumberToIncrement)
-        
-        let parentDiv = clickedBtn.closest('.score-section')
-        console.log('Parent div : ', parentDiv)
+      let parentDiv = clickedBtn.closest(".score-section");
+      console.log("Parent div : ", parentDiv);
 
-        if(parentDiv.id === "home"){
+      if (parentDiv.id === "home") {
+        homeScore += NumberToIncrement;
+        parentDiv.querySelector(".score").textContent = homeScore;
+      } else if (parentDiv.id === "guest") {
+        guestScore += NumberToIncrement;
+        parentDiv.querySelector(".score").textContent = guestScore;
+      }
 
-            homeScore += NumberToIncrement
-            parentDiv.querySelector('.score').textContent = homeScore
+      updateLeaderStyles();
+    });
+  });
 
-        }
-        else if(parentDiv.id === "guest"){
+  resetScoreBtn.addEventListener("click", function () {
+    homeScore = 0;
+    guestScore = 0;
 
-            guestScore += NumberToIncrement
-            parentDiv.querySelector('.score').textContent = guestScore
-
-        }
-
-        updateLeaderStyles()
-
-    })
-
-})
-
-resetScoreBtn.addEventListener('click', function(){
-
-    homeScore = 0
-    guestScore = 0
-
-    let scoreSections = document.querySelectorAll('.score-section')
+    let scoreSections = document.querySelectorAll(".score-section");
 
     scoreSections.forEach((section) => {
-        section.querySelector('.score').textContent = 0
-    })
+      section.querySelector(".score").textContent = 0;
+    });
 
-    updateLeaderStyles()
-})
+    updateLeaderStyles();
+  });
+});
